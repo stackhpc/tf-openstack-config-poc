3085f0c202c96667f43f5bc348ad06f29f77ada2
Steve Brasier <steveb@stackhpc.com>
2026-01-29 14:57:54 +0000

    add key manipulation + optional json

--- datadriven-migrate.py
+++ datadriven-migrate.py
@@ -3,22 +3,27 @@
 import sys, subprocess, json, pprint
 
 class OSCmd:
-    def __init__(self, cmdstr):
+    def __init__(self, cmdstr, as_json=True):
         self.cmdstr = cmdstr
+        self.as_json = as_json
+        self.value = None
     def __call__(self, **args):
-
         expanded_cmd = self.cmdstr.format(**args)
-        cmd_list = ["openstack"] + expanded_cmd.split() + ["-f", "json"]
+        cmd_list = ["openstack"] + expanded_cmd.split()
+        cmd_list += ["-f", "json"] if self.as_json else ["-f", "value"]
         print('calling', cmd_list)
         p = subprocess.run(cmd_list, text=True, capture_output=True)
         # todo: error handling!
-        return json.loads(p.stdout)
+        self.value = json.loads(p.stdout) if self.as_json else p.stdout.strip()
+
+    def __repr__(self):
+        return pprint.pformat(self.value)
 
 project = {
-    "description": OSCmd("project show {project_name} -c description"),
-    "quotas": {
-        "compute": OSCmd("quota show --compute {project_name}")
-        }
+    "{project_name}": {
+        "description": OSCmd("project show {project_name} -c description", as_json=False),
+        "compute_quotas": OSCmd("quota show --compute {project_name}")
+    }
 }
 
 def walk(obj, args=None, method='__call__'):
@@ -39,6 +44,7 @@ def walk(obj, args=None, method='__call__'):
     
     while queue:
         current = queue.pop(0)
+        #print('current', current)
 
         if id(current) in visited:
             continue
@@ -46,18 +52,24 @@ def walk(obj, args=None, method='__call__'):
 
         if isinstance(current, dict):
             for key, value in list(current.items()):
+                fmt_key = key.format(**args)
+                if fmt_key != key:
+                    del current[key]
+                    current[fmt_key] = value
                 if isinstance(value, OSCmd):
-                    current[key] = getattr(value, method)(**args)
+                    getattr(value, method)(**args)
                 elif isinstance(value, (dict, list)):
                     queue.append(value)
 
         elif isinstance(current, list):
+            print('processing list')
             for i, value in enumerate(current):
                 if isinstance(value, OSCmd):
-                    current[i] = getattr(value, method)(**args)
+                    getattr(value, method)(**args)
                 elif isinstance(value, (dict, list)):
                     queue.append(value)
-    
+        else:
+            print('should be an error?')
     return obj
 
 
